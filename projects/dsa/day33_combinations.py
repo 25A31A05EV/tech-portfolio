f79cbb023892d749e5e0eeda628f22cf65135fb1
Sowmya <sowmyavelugubantla5@gmail.com>
2026-08-14 05:40:52 +0530

    Day33: Combinations - Backtracking pattern

---
+++ projects/dsa/day33_combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        result = []
+        path = []
+
+        def backtrack(start):
+            if len(path) == k:
+                result.append(path.copy())
+                return
+
+            for i in range(start, n + 1):
+                path.append(i)
+                backtrack(i + 1)
+                path.pop()
+
+        backtrack(1)
+        return result

17d6a353457277c838db14c9f837bd97df63cf8f
Sowmya <sowmyavelugubantla5@gmail.com>
2026-09-15 18:44:21 +0530

    Day 64: House Robber - DP with max() choice pattern

---
+++ projects/dsa/day64_house_robber.py
@@ -0,0 +1,13 @@
+def rob(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    
+    prev2, prev1 = 0, 0
+    for num in nums:
+        current = max(prev1, prev2 + num)
+        prev2 = prev1
+        prev1 = current
+    
+    return prev1
